f74e5893fcaabdfd1d34829dcc97abd6355f1347
Gagandeep Singh <singh.enso@gmail.com>
2017-08-17 11:55:03 -0700

    update with new style - latest edits with hover over effects on quicklinks

--- template-home-page.php
+++ template-home-page.php
@@ -10,16 +10,18 @@
 <?php get_template_part('templates/component', 'quick-link'); ?>
 <div class="home-content">
     <div class="content-wrapper">
-        <div class="col-md-8 left-column">
-            <?php echo '<h2>' . get_field('left_column_header') . '</h2>';
-                echo get_field('left_column_content');
+    <div class="row">
+    <div class="col-md-12 padding-30px">
+            <?php echo '<h2>' . get_field('first_row_header') . '</h2>';
+                echo get_field('first_row_content');
+            ?>
+            </div>
+    </div>
+    <div class="row">
+            <?php 
+              echo get_field('bottom_row_content');
             ?>
         </div>
-        <div class="col-md-4 right-column">
-            <?php echo '<img class="img-responsive" src="' . get_field('right_column_image') . '"/>';
-              echo '<h2>' . get_field('right_column_heading') . '</h2>';
-              echo get_field('right_column_content');
-            cool?>
         </div>
     </div>
 </div>
--- templates/component-quick-link.php
+++ templates/component-quick-link.php
@@ -3,10 +3,11 @@
 if ($quicklinks) {
     echo '<div class="quick-links"><div class="page-container">';
     foreach ($quicklinks as $content ) {
-        echo '<div class="col-sm-3 center-block text-center">
-        <i class="fa fa-' . $content['icon'] . ' quick-link-icon"></i>
-        <p>' . $content['content'] . '</p>
-        <a href="' . $content['read_more'] . '">Read More <i class="fa fa-chevron-right" aria-hidden="true"></i></a>
+        echo '<div class="col-sm-3 center-block  text-center">
+        <a  class="quicklink-block-hover" href="' . $content['read_more'] . '">
+        <i class="' . $content['icon'] . ' quick-link-icon"></i>
+        <p class="quicklink-content-p">' . $content['content'] . '</p>
+        <span class="ql-anchor"php>Read More <i class="fa fa-chevron-right" aria-hidden="true"></i></a>
         </div>';
     }
     echo '</div></div>';
