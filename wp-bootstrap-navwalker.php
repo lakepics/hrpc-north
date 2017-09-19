eba8d960b2250fd3ec22f5a5632285839a242c32
Gagandeep Singh <singh.enso@gmail.com>
2017-09-19 11:19:48 -0700

    working dropdown for tablet and clean css for mobile

--- wp-bootstrap-navwalker.php
+++ wp-bootstrap-navwalker.php
@@ -139,7 +139,7 @@ if ( ! class_exists( 'WP_Bootstrap_Navwalker' ) ) {
 					$item_output .= '<a' . $attributes . '>';
 				endif;
 				$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
-				$item_output .= ( $args->has_children && 0 === $depth ) ? ' <span class="caret"></span></a>' : '</a>';
+				$item_output .= ( $args->has_children && 0 === $depth ) ? ' </a>' : '</a>';
 				$item_output .= $args->after;
 				$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
 			}
